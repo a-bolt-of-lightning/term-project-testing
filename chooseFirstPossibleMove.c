6309d25862855c40f72f0fee04a392a803ef870d
a-bolt-of-lightning <teamoonshine@gmail.com>
2019-01-25 12:49:43 +0330

    strategy almost completed. needs editing

--- chooseFirstPossibleMove.c
+++ chooseFirstPossibleMove.c
@@ -8,8 +8,13 @@
 #define WHITE 2
 
 //function prototypes below:
+
+void find_and_print_the_best_move(int const possible_moves_arr[][4], const int possible_moves_counter);
+
+//
+int flippedDisk(int const square[ROW][COLUMN], int i, int j, int const player, int const opponent, int x, int y);
 //function to set a fixed worth for possible moves
-void set_square_worth(const int square[ROW][COLUMN]);
+void set_square_worth(const int square[ROW][COLUMN], int const player, int const opponent);
 //function(s) to find the possible moves for player 
 void possible_moves_eastward(const int opponent, const int player, int i,int j, int square[ROW][COLUMN]);
 void possible_moves_westward(const int opponent, const int player, int j,int i, int square[ROW][COLUMN]);
@@ -33,7 +38,6 @@ int main( int argc, char * argv[] )
     
     else if (player==WHITE)
         opponent=BLACK;
-
 
     //constructs the board
     for(i=0; i<ROW; i++)
@@ -59,11 +63,10 @@ int main( int argc, char * argv[] )
                 if(square[i-1][j-1]==opponent) possible_moves_northwestward(opponent, player, j-1, i-1, square);
                 if(square[i+1][j+1]==opponent) possible_moves_southeastward(opponent, player, j+1, i+1, square);
                 if(square[i+1][j-1]==opponent) possible_moves_southwestward(opponent, player, j-1, i+1, square);
-
-
             }
         }
     }
+    
     /*
     for(i=0; i<ROW; i++)
     {
@@ -73,9 +76,9 @@ int main( int argc, char * argv[] )
         }
         putchar('\n');
     }
-    printf("%d\n", player);
+    //printf("%d\n", player);
     */
-    set_square_worth(square);
+    set_square_worth(square, player, opponent);
     return 0;
 }
 
@@ -116,7 +119,6 @@ void possible_moves_westward(const int opponent, const int player, int j,int i, 
     }
     return;
 }
-
 
 void possible_moves_northward(const int opponent, const int player, int j,int i, int square[ROW][COLUMN])
 {
@@ -176,7 +178,6 @@ void possible_moves_northeastward(const int opponent, const int player, int j,in
     return;
 }
 
-
 void possible_moves_northwestward(const int opponent, const int player, int j,int i, int square[ROW][COLUMN])
 {
     while(j<ROW && i<COLUMN && i>=0 && j>=0)
@@ -196,7 +197,6 @@ void possible_moves_northwestward(const int opponent, const int player, int j,in
     }
     return;
 }
-
 
 void possible_moves_southeastward(const int opponent, const int player, int j,int i, int square[ROW][COLUMN])
 {
@@ -218,7 +218,6 @@ void possible_moves_southeastward(const int opponent, const int player, int j,in
     return;
 }
 
-
 void possible_moves_southwestward(const int opponent, const int player, int j,int i, int square[ROW][COLUMN])
 {
     while(j<ROW && i<COLUMN && i>=0 && j>=0)
@@ -239,12 +238,13 @@ void possible_moves_southwestward(const int opponent, const int player, int j,in
     return;
 }
 
-void set_square_worth(const int square[ROW][COLUMN])
+void set_square_worth(const int square[ROW][COLUMN], int const player, int const opponent)
 {
     int i, j;
 
     //fixed worth of each square
-    int const squareWorth[ROW][COLUMN]= {{99, -8, 8, 6, 6, 8, -8, 99},
+    int const squareWorth[ROW][COLUMN]= 
+    {{99, -8, 8, 6, 6, 8, -8, 99},
     {-8 -24, -4, -3, -3, -4, -24, -8},
     {8, -4, 7, 4, 4, 7, -4, 8},
     {6, -3, 4, 0, 0, 4, -3, 6},
@@ -280,12 +280,7 @@ void set_square_worth(const int square[ROW][COLUMN])
             }
         }
     }
-    /*
-    for(i=0; i<possible_moves_counter; i++)
-    {
-        printf(" %d, %d, %d, %d\n ", possible_moves_arr[i][0], possible_moves_arr[i][1], possible_moves_arr[i][2], possible_moves_arr[i][3]);
-    }
-    */
+    
     int max_worth=possible_moves_arr[0][2];
     int x=possible_moves_arr[0][1], y=possible_moves_arr[0][0];
     for(i=0; i<possible_moves_counter; i++)
@@ -298,9 +293,87 @@ void set_square_worth(const int square[ROW][COLUMN])
         }    
     }
 
+    //printf("%d %d", x, y);
+
+    for(i=0; i<possible_moves_counter; i++ )
+    {
+        if(possible_moves_arr[i][2]==max_worth)
+        {
+            possible_moves_arr[i][3]=
+            flippedDisk(square, 0, 1, player, opponent, possible_moves_arr[i][1], possible_moves_arr[i][0])+
+            flippedDisk(square, 0, -1, player, opponent, possible_moves_arr[i][1], possible_moves_arr[i][0])+
+            flippedDisk(square, 1, 0, player, opponent, possible_moves_arr[i][1], possible_moves_arr[i][0])+
+            flippedDisk(square, -1, 0, player, opponent, possible_moves_arr[i][1], possible_moves_arr[i][0])+
+            flippedDisk(square, 1, 1, player, opponent, possible_moves_arr[i][1], possible_moves_arr[i][0])+
+            flippedDisk(square, -1, 1, player, opponent, possible_moves_arr[i][1], possible_moves_arr[i][0])+
+            flippedDisk(square, 1, -1, player, opponent, possible_moves_arr[i][1], possible_moves_arr[i][0])+
+            flippedDisk(square, -1, -1, player, opponent, possible_moves_arr[i][1], possible_moves_arr[i][0]);
+        }
+    }
+
+    //something to find the move with least flips
+    find_and_print_the_best_move(possible_moves_arr, possible_moves_counter);
+    
+    /*
+    for(i=0; i<possible_moves_counter; i++)
+    {
+        printf(" %d, %d, %d, %d\n ", possible_moves_arr[i][0], possible_moves_arr[i][1], possible_moves_arr[i][2], possible_moves_arr[i][3]);
+    }
+    */
+}
+
+int flippedDisk(int const square[ROW][COLUMN], int i, int j, int const player, int const opponent, int x, int y)
+{
+    int i_counter=i, j_counter=j;
+    int disk_counter=0;
+    //printf("%d %d \n", x, y);
+    x=x+j;
+    y=y+i;
+    while(y<ROW && y>=0 && x<COLUMN && x>=0)
+    {
+        //printf("%d inside while %d %d\n", disk_counter, x, y);
+
+        if((square[y][x]!=opponent && square[y][x]!=player))
+            
+            {
+                //printf("%d while cond\n", disk_counter);
+                 return 0;
+            }   
+        
+        if(square[y][x]==opponent)
+        {
+            disk_counter++;
+            //printf("opponent %d\n", disk_counter);
+        }
+            
+        if(square[y][x]==player)
+        {
+            //printf("player %d\n", disk_counter);
+            return disk_counter;
+        }
+            
+        x=x+j;
+        y=y+i;
+    }
+    //printf("%d outta while %d %d\n", disk_counter, x, y);
+    return 0;
+}
+
+void find_and_print_the_best_move(int const possible_moves_arr[][4], const int possible_moves_counter)
+{
+    int i,x, y, min_flip=64;
+    for(i=0; i<possible_moves_counter; i++)
+    {
+        if(possible_moves_arr[i][3]!=0)
+        {
+            if(possible_moves_arr[i][3]<min_flip)
+            {
+                min_flip=possible_moves_arr[i][3];
+                x=possible_moves_arr[i][1];
+                y=possible_moves_arr[i][0];
+            }     
+        }
+    }
+
     printf("%d %d", x, y);
-
-    // void func_to_count_flipped_disks_for_each_max_worth_move()
-
-
 }
